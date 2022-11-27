c2ca9972d70bca98c9e04f76540ccbda2ec1d554
Annikatti <annika.tikki@hotmail.com>
2022-11-27 18:36:56 +0200

    lisää asiakas formi ja validointi

--- Asiakas/src/main/webapp/index.jsp
+++ Asiakas/src/main/webapp/index.jsp
@@ -5,9 +5,48 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="listaaAsiakkaat.jsp">Kaikki asiakkaat</a>
+<form id="uusi-asiakas" onsubmit="lisaaAsiakas()">
+    <h2>Uuden asiakkaan lisäys</h2>
+
+    <label for="etunimi">Etunimi:
+        <input id="etunimi" name="etunimi" type="text">
+        <output id="etunimi-error" class="failure hidden">Etunimi täytyy antaa</output>
+    </label>
+
+    <label for="sukunimi">Sukunimi:
+        <input id="sukunimi" name="sukunimi" type="text">
+        <output id="sukunimi-error" class="failure hidden">Sukunmi täytyy antaa</output>
+    </label>
+
+    <label for="puhelin">Puhelinnumero:
+        <input id="puhelin" name="puhelin" type="tel">
+        <output id="puhelin-error" class="failure hidden">Puhelinnumeron tulee olla muodossa 1234567</output>
+    </label>
+
+    <label for="sposti">Sähköpostiosoite:
+        <input id="sposti" name="sposti" type="email">
+        <output id="sposti-error" class="failure hidden">Tarkista sähköposti</output>
+    </label>
+
+    <button type="submit">Lisää uusi asiakas</button>
+</form>
+
+<h2>Asiakaslista</h2>
+<table>
+    <thead>
+    <tr>
+        <td>Etunimi</td>
+        <td>Sukunimi</td>
+        <td>Puhelinnumero</td>
+        <td>Sähköpostiosoite</td>
+    </tr>
+    </thead>
+    <tbody id="tbody"></tbody>
+</table>
+
+<script>
+    formListeners();
+    haeAsiakkaat();
+</script>
 </body>
 </html>
--- Asiakas/src/main/webapp/scripts/main.js
+++ Asiakas/src/main/webapp/scripts/main.js
@@ -1,3 +1,84 @@
+function formListeners() {
+    const form = document.getElementById('uusi-asiakas');
+    form.addEventListener('submit', (e) => e.preventDefault());
+
+    const etunimi = document.getElementById('etunimi');
+    etunimi.valid = false;
+    etunimi.addEventListener('input', () => tarkistaNimi(etunimi));
+
+    const sukunimi = document.getElementById('sukunimi');
+    sukunimi.valid = false;
+    sukunimi.addEventListener('input', () => tarkistaNimi(sukunimi));
+
+    const puhelin = document.getElementById('puhelin');
+    puhelin.valid = false;
+    puhelin.addEventListener('input', () => tarkistaPuhelin(puhelin));
+
+    const sposti = document.getElementById('sposti');
+    sposti.valid = false;
+    sposti.addEventListener('input', () => tarkistaSposti(sposti));
+}
+
+//Funktio XSS-hyökkäysten estämiseksi (Cross-site scripting)
+function siivoa(teksti){
+    teksti=teksti.replace(/</g, "");//&lt;
+    teksti=teksti.replace(/>/g, "");//&gt;
+    teksti=teksti.replace(/'/g, "''");//&apos;
+    return teksti;
+}
+
+function tarkistaNimi(element) {
+    element.value = siivoa(element.value);
+    if (element.value.length > 0) {
+        validInput(element);
+        return;
+    }
+
+    invalidInput(element);
+}
+
+function tarkistaPuhelin(element) {
+    element.value = siivoa(element.value);
+    if (element.value.length === 7) {
+        validInput(element);
+        return;
+    }
+
+    invalidInput(element);
+}
+
+function tarkistaSposti(element) {
+    element.value = siivoa(element.value);
+    const emailRegexp = new RegExp('(?:[a-z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\n' +
+        '\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\n' +
+        '\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:\n' +
+        '(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])');
+
+    if (emailRegexp.test(element.value)) {
+        validInput(element);
+        return;
+    }
+
+    invalidInput(element);
+}
+
+function validInput(input) {
+    input.valid = true;
+    input.classList.remove('failure');
+    input.classList.add('success');
+
+    const error = document.getElementById(`${input.id}-error`);
+    error.classList.add('hidden');
+}
+
+function invalidInput(input) {
+    input.valid = false;
+    input.classList.add('failure');
+
+    const error = document.getElementById(`${input.id}-error`);
+    error.classList.remove('hidden');
+}
+
 //funktio tietojen hakemista varten. Kutsutaan backin GET metodia
 function haeAsiakkaat() {
     let url = "Asiakkaat";
